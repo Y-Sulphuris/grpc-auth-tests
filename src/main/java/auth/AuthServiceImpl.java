9b3e755f000ea3cc99fe4802e755e88fd5ef1fd9
Sulphuris <q.mindubaev@gmail.com>
2025-05-31 23:35:49 +0300

    Initial commit

---
+++ src/main/java/auth/AuthServiceImpl.java
@@ -0,0 +1,29 @@
+package com.ydo4ki;
+
+import io.grpc.stub.StreamObserver;
+
+/**
+ * @since 5/31/2025 10:43 PM
+ */
+public class AuthServiceImpl extends AuthServiceGrpc.AuthServiceImplBase {
+	private final UserDatabaseServiceGrpc.UserDatabaseServiceBlockingStub dbStub;
+	
+	public AuthServiceImpl(UserDatabaseServiceGrpc.UserDatabaseServiceBlockingStub dbStub) {
+		this.dbStub = dbStub;
+	}
+	
+	@Override
+	public void register(UserRequest request, StreamObserver<UserResponse> responseObserver) {
+		if (request.getEmail().isEmpty()) {
+			responseObserver.onNext(UserResponse.newBuilder().setStatus("Email is required").build());
+			responseObserver.onCompleted();
+			return;
+		}
+		
+		UserResponse dbResponse = dbStub.saveUser(request);
+		
+		responseObserver.onNext(dbResponse);
+		responseObserver.onCompleted();
+	}
+}
+
---
+++ src/test/java/auth/AuthServiceTest.java
@@ -0,0 +1,7 @@
+package auth;
+
+/**
+ * @since 5/31/2025 11:20 PM
+ */
+public class AuthServiceTest {
+}
